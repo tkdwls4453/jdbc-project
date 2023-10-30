f8a0c8eba16f24ae6c8582c57a96c1273ead128d
Yoon321 <tkdwls734@gmail.com>
2023-10-30 13:12:35 +0900

    직원 정보 관련 Dto 생성

---
+++ src/EmployeeDto.java
@@ -0,0 +1,125 @@
+public class EmployeeDto {
+    private String fname;
+    private String minit;
+    private String lname;
+    private String ssn;
+    private String bdate;
+    private String address;
+    private Sex sex;
+    private Double salary;
+    private String supervisorName;
+    private Long dName;
+    private String created;
+    private String modified;
+
+    public EmployeeDto(String fname, String minit, String lname, String ssn, String bdate, String address, Sex sex, Double salary, String supervisorNmae, Long dName, String created, String modified) {
+        this.fname = fname;
+        this.minit = minit;
+        this.lname = lname;
+        this.ssn = ssn;
+        this.bdate = bdate;
+        this.address = address;
+        this.sex = sex;
+        this.salary = salary;
+        this.supervisorName = supervisorNmae;
+        this.dName = dName;
+        this.created = created;
+        this.modified = modified;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getMinit() {
+        return minit;
+    }
+
+    public void setMinit(String minit) {
+        this.minit = minit;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getBdate() {
+        return bdate;
+    }
+
+    public void setBdate(String bdate) {
+        this.bdate = bdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getSupervisorName() {
+        return supervisorName;
+    }
+
+    public void setSupervisorName(String supervisorName) {
+        this.supervisorName = supervisorName;
+    }
+
+    public Long getdName() {
+        return dName;
+    }
+
+    public void setdName(Long dName) {
+        this.dName = dName;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+}
