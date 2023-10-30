ae4b9f84cec22079093ab7c583dcdabf3f453d57
Yoon321 <tkdwls734@gmail.com>
2023-10-30 17:40:41 +0900

    EmployeeDto 필요없는 필드 삭제 및 변경

--- src/EmployeeDto.java
+++ src/EmployeeDto.java
@@ -1,125 +1,66 @@
 public class EmployeeDto {
-    private String fname;
-    private String minit;
-    private String lname;
+    private String name;
     private String ssn;
     private String bdate;
     private String address;
-    private Sex sex;
-    private Double salary;
+    private String sex;
+    private String salary;
     private String supervisorName;
-    private Long dName;
-    private String created;
-    private String modified;
+    private String dname;
 
-    public EmployeeDto(String fname, String minit, String lname, String ssn, String bdate, String address, Sex sex, Double salary, String supervisorNmae, Long dName, String created, String modified) {
-        this.fname = fname;
-        this.minit = minit;
-        this.lname = lname;
-        this.ssn = ssn;
+    public EmployeeDto(String name, String ssn, String bdate, String address, String sex, String salary, String supervisorNmae, String dName) {
+        this.name = name;
         this.bdate = bdate;
         this.address = address;
         this.sex = sex;
         this.salary = salary;
         this.supervisorName = supervisorNmae;
-        this.dName = dName;
-        this.created = created;
-        this.modified = modified;
+        this.dname = dName;
     }
 
-    public String getFname() {
-        return fname;
-    }
-
-    public void setFname(String fname) {
-        this.fname = fname;
-    }
-
-    public String getMinit() {
-        return minit;
-    }
-
-    public void setMinit(String minit) {
-        this.minit = minit;
-    }
-
-    public String getLname() {
-        return lname;
-    }
-
-    public void setLname(String lname) {
-        this.lname = lname;
+    public String getName() {
+        return name;
     }
 
     public String getSsn() {
         return ssn;
     }
 
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
-
     public String getBdate() {
         return bdate;
-    }
-
-    public void setBdate(String bdate) {
-        this.bdate = bdate;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Sex getSex() {
+    public String getSex() {
         return sex;
     }
 
-    public void setSex(Sex sex) {
-        this.sex = sex;
-    }
-
-    public Double getSalary() {
+    public String getSalary() {
         return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
     }
 
     public String getSupervisorName() {
         return supervisorName;
     }
 
-    public void setSupervisorName(String supervisorName) {
-        this.supervisorName = supervisorName;
+    public String getDname() {
+        return dname;
     }
 
-    public Long getdName() {
-        return dName;
-    }
-
-    public void setdName(Long dName) {
-        this.dName = dName;
-    }
-
-    public String getCreated() {
-        return created;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
-
-    public String getModified() {
-        return modified;
-    }
-
-    public void setModified(String modified) {
-        this.modified = modified;
+    @Override
+    public String toString() {
+        return "EmployeeDto{" +
+                "name='" + name + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", bdate='" + bdate + '\'' +
+                ", address='" + address + '\'' +
+                ", sex='" + sex + '\'' +
+                ", salary='" + salary + '\'' +
+                ", supervisorName='" + supervisorName + '\'' +
+                ", dname='" + dname + '\'' +
+                '}';
     }
 }
